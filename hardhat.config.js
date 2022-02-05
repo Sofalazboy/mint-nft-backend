4d91ac2b1596db96a0a3bf7db45761f2e00d44b2
Sofa Lazboy <sofalazboy@gmail.com>
2022-02-05 18:43:39 +0200

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
+require("@nomiclabs/hardhat-etherscan");
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    rinkeby: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }
+};
+
+
